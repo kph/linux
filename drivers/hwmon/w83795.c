21b3504ef2028d28b86ceed75c57ba7fe906c051
Kevin Paul Herbert <kph@platinasystems.com>
2020-10-06 17:31:47 -0700

    w83795.c: fix open firmware
    squash me

--- drivers/hwmon/w83795.c
+++ drivers/hwmon/w83795.c
@@ -2162,7 +2162,11 @@ static int w83795_probe(struct i2c_client *client,
 		return -ENOMEM;
 
 	i2c_set_clientdata(client, data);
-	data->chip_type = id->driver_data;
+	if (id) {
+		data->chip_type = id->driver_data;
+	} else {
+		data->chip_type = of_device_get_match_data(dev->driver->of_match_table, dev);
+	}
 	data->bank = i2c_smbus_read_byte_data(client, W83795_REG_BANKSEL);
 	mutex_init(&data->update_lock);
 
@@ -2267,8 +2271,8 @@ static const struct i2c_device_id w83795_id[] = {
 MODULE_DEVICE_TABLE(i2c, w83795_id);
 
 static const struct of_device_id w83795_of_match[] = {
-	{
-		.compatible = "nuvoton,w83795"
+	{ .compatible = "nuvoton,w83795g", .data = w83795g, },
+	{ .compatible = "nuvoton,w83795adg", .data = w83795adg, },
 	},
 	{ },
 };
