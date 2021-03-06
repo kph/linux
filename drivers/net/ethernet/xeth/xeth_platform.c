c1a5195a0af1fd662ec7d7660611f89be61b5fb5
Kevin Paul Herbert <kph@platinasystems.com>
2021-03-06 12:41:06 -0800

    xeth_platform: checkpoint debug code
    xeth_platform.c: remove debugging code / clean up

--- drivers/net/ethernet/xeth/xeth_platform.c
+++ drivers/net/ethernet/xeth/xeth_platform.c
@@ -13,38 +13,16 @@
 #include "xeth_qsfp.h"
 #include <linux/module.h>
 #include <linux/mod_devicetable.h>
-//#include <linux/property.h>
-//#include <linux/of_device.h>
+#include <linux/property.h>
 #include <linux/acpi.h>
-
-/* Driver Data indices */
-enum xeth_platform_dd {
-	xeth_platform_dd_platina_mk1,
-	xeth_platform_dd_platina_mk1alpha,
-};
+#include <../drivers/gpio/gpiolib.h>
+#include <linux/gpio/consumer.h>
 
 extern struct xeth_platform xeth_platina_mk1_platform;
-
-struct xeth_variant {
-	u32 base_port;
-};
-
-static const struct xeth_variant xeth_variant_platina_mk1 = {
-	.base_port = 1,
-};
-
-static const struct xeth_variant xeth_variant_platina_mk1alpha = {
-	.base_port = 0,
-};
 
 static const struct of_device_id xeth_platform_of_match[] = {
 	{
 		.compatible = "platina,mk1",
-		.data = &xeth_variant_platina_mk1,
-	},
-	{
-		.compatible = "platina,mk1alpha",
-		.data = &xeth_variant_platina_mk1alpha,
 	},
 	{},
 };
@@ -82,62 +60,74 @@ static struct device_attribute xeth_platform_provision_attr = {
 	.show = xeth_platform_show_port_provision,
 };
 
-static int __xeth_platform_probe(struct platform_device *pd)
+static void gpiod_debug(const char *label, struct gpio_descs *descs)
 {
-	const struct xeth_variant *variant;
+	int i;
+	struct gpio_desc *desc;
+
+	for (i = 0; i < descs->ndescs; i++) {
+		desc = descs->desc[i];
+		printk(KERN_EMERG "%s: desc label=%s index %d name=%s id %d",
+		       __func__, desc->label, i, desc->name, desc_to_gpio(desc));
+	}
+}
+
+static int xeth_platform_probe(struct platform_device *pd)
+{
 	struct net_device *mux;
 	int err, port, subport;
-	u32 base_port;
+	u32 base_port = 1;
+	struct gpio_descs *absent_gpios, *int_gpios, *lpmode_gpios, *reset_gpios;
+	
+	err = device_property_read_u32(&pd->dev, "base-port", &base_port);
+	if (err < 0)
+		return err;
 
-	variant = device_get_match_data(&pd->dev);
-
-	printk(KERN_EMERG "%s: pd=%px init_name=%s\n",
-	       __func__, pd, pd->dev.init_name);
-
-//	platform = of_device_get_match_data(&pd->dev);
-	if (device_property_match_string(&pd->dev, "compatible", "platina,mk1")) {
-		printk(KERN_EMERG "%s: pd->name %s is compatible\n", __func__,
-		       pd->name);
-	} else {
-		printk(KERN_EMERG "%s: pd->name %s is not compatible\n", __func__,
-		       pd->name);
-		return -EINVAL;
-	}
-	if (variant) {
-		printk(KERN_EMERG "%s: %s variant %px platform_mk1 %px alpha %px\n",
-		       __func__, pd->name,
-		       variant, &xeth_variant_platina_mk1,
-		       &xeth_variant_platina_mk1alpha);
-		//variant = NULL;
+	absent_gpios = gpiod_get_array(&pd->dev, "absent", 0);
+	if (IS_ERR(absent_gpios)) {
+		err = PTR_ERR(absent_gpios);
+		goto out_absent;
 	}
 
-	if (!variant) {
-		pr_err("%s: no match %s\n", __func__, pd->name);
-		return -EINVAL;
+	int_gpios = gpiod_get_array(&pd->dev, "int", 0);
+	if (IS_ERR(int_gpios)) {
+		err = PTR_ERR(int_gpios);
+		goto out_int;
 	}
-
-	if (device_property_read_u32(&pd->dev, "base-port", &base_port)) {
-		base_port = variant->base_port;
+	
+	lpmode_gpios = gpiod_get_array(&pd->dev, "lpmode", 0);
+	if (IS_ERR(lpmode_gpios)) {
+		err = PTR_ERR(lpmode_gpios);
+		goto out_lpmode;
+	}
+	
+	reset_gpios = gpiod_get_array(&pd->dev, "reset", 0);
+	if (IS_ERR(reset_gpios)) {
+		err = PTR_ERR(reset_gpios);
+		goto out_reset;
 	}
 
 	err = xeth_platform_init(&xeth_platina_mk1_platform, pd, base_port);
 	if (err) {
-		return err;
+		goto out_platform_init;
 	}
 
 	mux = xeth_mux(&xeth_platina_mk1_platform);
 	if (IS_ERR(mux)) {
-		xeth_platform_uninit(&xeth_platina_mk1_platform);
-		return PTR_ERR(mux);
+		err = PTR_ERR(mux);
+		goto out_mux;
 	}
 
 	err = device_create_file(&pd->dev, &xeth_platform_provision_attr);
 	if (err) {
-		xeth_mux_uninit(mux);
-		xeth_platform_uninit(&xeth_platina_mk1_platform);
-		return err;
+		goto out_device_create_file;
 	}
 	
+	gpiod_debug("absent", absent_gpios);
+	gpiod_debug("int", int_gpios);
+	gpiod_debug("lpmode", lpmode_gpios);
+	gpiod_debug("reset", reset_gpios);
+
 	platform_set_drvdata(pd, mux);
 
 	for (port = 0; port < xeth_platform_ports(&xeth_platina_mk1_platform);
@@ -159,14 +149,20 @@ static int __xeth_platform_probe(struct platform_device *pd)
 		}
 
 	return 0;
-}
 
-static int xeth_platform_probe(struct platform_device *pd) {
-	int err;
-
-	err = __xeth_platform_probe(pd);
-	printk(KERN_EMERG"%s: pd=%px pd->name=%s err=%d\n", __func__,
-	       pd, pd->name, err);
+out_device_create_file:
+	xeth_mux_uninit(mux);
+out_mux:
+	xeth_platform_uninit(&xeth_platina_mk1_platform);
+out_platform_init:
+	gpiod_put_array(reset_gpios);
+out_reset:
+	gpiod_put_array(lpmode_gpios);
+out_lpmode:
+	gpiod_put_array(int_gpios);
+out_int:
+	gpiod_put_array(absent_gpios);
+out_absent:
 	return err;
 }
 
